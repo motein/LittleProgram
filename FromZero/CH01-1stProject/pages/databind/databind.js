05b8aebeb6a0e335104504aac6781c32202a5b6b
motein <motein@qq.com>
2019-09-19 01:04:31 +0800

    Add data bind Tab.

---
+++ FromZero/CH01-1stProject/pages/databind/databind.js
@@ -0,0 +1,74 @@
+// pages/databind/databind.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    content: '微信小程序数据绑定内容',
+    hiddencontent: '隐藏内容',
+    flag: false,
+    num1: 1,
+    num2: 2,
+    user: {
+      name: "spark",
+      age: 18
+    }
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
