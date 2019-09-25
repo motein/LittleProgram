e0c63177700e8531d3ce2a01f6d249b287e56350
motein <motein@qq.com>
2019-09-26 00:04:46 +0800

    Add CH07-02

--- FromZero/CH07-2nd/pages/action-sheet/action-sheet.js
+++ FromZero/CH07-2nd/pages/action-sheet/action-sheet.js
@@ -1,66 +1,51 @@
-// pages/action-sheet/action-sheet.js
-Page({
+var items = ['菜单1', '菜单2', '菜单3', '菜单4']
+
+var pageObject = {
 
   /**
    * Page initial data
    */
   data: {
-
+    actionSheetHidden: true,
+    actionSheetItems: items
   },
-
-  /**
-   * Lifecycle function--Called when page load
-   */
-  onLoad: function (options) {
-
+  actionSheetTap: function(e) {
+    this.setData({
+      actionSheetHidden: false
+    })
   },
-
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
-  onReady: function () {
-
+  actionSheetTap2: function (e) {
+    wx.showActionSheet({
+      itemList: items,
+      success: function(res) {
+        console.log("success")
+        console.log(res)
+        if (!res.cancel) {
+          console.log(res.tapIndex)
+        }
+      },
+      complete: function(e) {
+        console.log("complete")
+        console.log(e)
+      }
+    })
   },
-
-  /**
-   * Lifecycle function--Called when page show
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page hide
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page unload
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * Called when page reach bottom
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage: function () {
-
+  actionSheetChange: function (e) {
+    this.setData({
+      actionSheetHidden: true
+    })
   }
-})
+}
+
+for (var i = 0; i < items.length; ++i) {
+  (function (itemName) {
+    pageObject['bind' + itemName] = function (e) {
+      console.log('click' + itemName)
+      console.log(e)
+      this.setData({
+        actionSheetHidden: true
+      })
+    }
+  })(items[i])
+}
+Page(pageObject)
